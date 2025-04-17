c59a0a6bd8fc4ad25ce0c4effc6002fa8de357ac
Enhbayr <enhbayr745@gmail.com>
2025-04-17 12:21:52 +0800

    ok

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,6 @@ const { createUser, signInUser, getInfo } = require("../controller/users");
 const { protect } = require("../middleware/protect");
 const router = express.Router();
 
-router.route("/").post(createUser).get(protect, getInfo)
-router.route("/").post(createUser).get(protect, getInfo)
+router.route("/").post(createUser).get(protect, getInfo);
 router.route("/signin").post(signInUser);
 module.exports = router;
